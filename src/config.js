e7f247c4ce6b4edbbe0bca2230a36cf83ac95a58
chanshiyu <me@chanshiyu.com>
2020-03-12 15:21:38 +0700

    fix: 环境判断

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const isDev = /^(192\.168|localhost)/.test(window.location.host)
 let baseURL, imWSURL
-if (!isDev) {
+if (isDev) {
   baseURL = 'http://localhost:8090'
   imWSURL = 'ws://localhost:7002/chat'
 } else {
